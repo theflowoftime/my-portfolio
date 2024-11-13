81db88a206a0f056abd6503a0dda365298c013ff
theflowoftime <daflowoftime@outlook.com>
2024-11-13 10:37:35 +0100

    vercel analytics & speed insights

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,8 @@ import Footer from "./components/footer";
 import Header from "./components/header";
 import Projects from "./components/projects";
 import useScrollHash from "./hooks/useScrollHash";
+import { SpeedInsights } from "@vercel/speed-insights/react";
+import { Analytics } from "@vercel/analytics/react";
 
 function App() {
   useScrollHash();
@@ -15,6 +17,8 @@ function App() {
       <Projects />
       <Contact />
       <Footer />
+      <SpeedInsights />
+      <Analytics />
     </div>
   );
 }
