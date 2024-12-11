e427eb86dc297d2a7f7330c8a127f038b1d6ac07
theflowoftime <daflowoftime@outlook.com>
2024-12-11 15:30:10 +0100

    comma conundurum

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -30,29 +30,18 @@ function Hero() {
 
   const [w, h] = heroData.avatarSize || HERO_AVATAR_SIZES;
 
-  // Function to create dynamic mask based on index and image width
   const generateMask = (
     wordIndex: number,
     isImage: boolean = false,
     lineIndex: number
   ) => {
     const numWords = heroData.mainTextLines.length;
-
-    // Image width in pixels
     const imageWidthPx = w;
-
-    // If it's an image, we adjust the offset by image width in pixels
     const offset = (wordIndex / numWords) * 100 + (isImage ? imageWidthPx : 0);
+    const baseOpacity = 1 - Math.min(offset / 100, 1);
+    const lineDimming = 1 - 0.1 * lineIndex;
+    const opacity = Math.max(baseOpacity * lineDimming, 0.4);
 
-    // Dimming effect: The further the element from the light source, the darker it becomes
-    const baseOpacity = 1 - Math.min(offset / 100, 1); // Opacity decreases as distance increases
-
-    // Dimming based on line index (lower lines = dimmer)
-    const lineDimming = 1 - 0.1 * lineIndex; // Each line gets progressively dimmer
-    const opacity = Math.max(baseOpacity * lineDimming, 0.4); // Ensure opacity never goes below 0.4 for readability
-    console.log(opacity);
-
-    // Generate the radial gradient with the adjusted opacity
     return `radial-gradient(150% 150% at ${offset}% 100%, rgba(0, 0, 0, ${opacity}) 0%, rgba(0, 0, 0, 1) 100%)`;
   };
 
@@ -66,7 +55,6 @@ function Hero() {
       className="relative flex flex-col justify-around h-full min-h-screen"
     >
       <motion.div className="container h-full text-center dark:text-white">
-        {/* Title Section */}
         <motion.div
           className={cn(
             "font-instrument font-medium text-[4rem] lg:text-[5.35rem] rounded-full flex flex-col items-center leading-tight",
@@ -75,115 +63,124 @@ function Hero() {
         >
           {heroData.mainTextLines.map((item, lineIndex) => {
             const words = item.line.text.split(" ");
-            // if (lineIndex === 0 && item.line.img.position < words.length) {
-            //   if (language === "AR") {
-            //     words.unshift(",");
-            //   } else {
-            //     words.push(",");
-            //   }
-            // }
-            const textWithImage = words.map((word, wordIndex) => (
-              <React.Fragment key={wordIndex}>
-                {wordIndex === item.line.img.position && (
+
+            const textWithImage = words.map((word, wordIndex) => {
+              if (lineIndex === 0 && item.line.img.position < words.length) {
+                if (language === "AR" && wordIndex === 0) {
+                  word = "," + word;
+                } else if (
+                  language !== "AR" &&
+                  wordIndex === words.length - 1
+                ) {
+                  word += ",";
+                }
+              }
+
+              return (
+                <React.Fragment key={wordIndex}>
+                  {wordIndex === item.line.img.position && (
+                    <Avatar
+                      className="hidden bg-black border-4 border-black dark:border-white sm:inline-block"
+                      style={{
+                        mask: generateMask(wordIndex, true, lineIndex),
+                        WebkitMask: generateMask(wordIndex, true, lineIndex),
+                        boxShadow: "rgba(100, 100, 111, 0.2) 0px 7px 29px 0px",
+                        width: w,
+                        height: h,
+                      }}
+                    >
+                      <Carousel
+                        plugins={[plugin.current]}
+                        opts={{
+                          loop: true,
+                          align: "start",
+                          axis: "y",
+                          containScroll: "trimSnaps",
+                          skipSnaps: true,
+                        }}
+                        orientation="vertical"
+                        onMouseEnter={plugin.current.stop}
+                        onMouseLeave={() => plugin.current.play()}
+                      >
+                        <CarouselContent style={{ height: h }}>
+                          {item.line.img.images?.map((image, index) => (
+                            <CarouselItem
+                              className={`p-0 m-0 cursor-grab`}
+                              key={index}
+                            >
+                              <AvatarImage
+                                className="object-contain w-full h-full overflow-hidden"
+                                src={urlFor(image)
+                                  .fit("max")
+                                  .quality(80)
+                                  .auto("format")
+                                  .url()}
+                                alt={item.line.img.altText}
+                              />
+                            </CarouselItem>
+                          ))}
+                        </CarouselContent>
+                      </Carousel>
+
+                      <AvatarFallback></AvatarFallback>
+                    </Avatar>
+                  )}
+
+                  {item.line.highlight?.includes(wordIndex) ? (
+                    <em className="italic dark:text-white text-black -tracking-[0.04em] -z-10">
+                      {word}
+                    </em>
+                  ) : (
+                    <span
+                      style={{
+                        mask: generateMask(wordIndex, false, lineIndex),
+                        WebkitMask: generateMask(wordIndex, false, lineIndex),
+                      }}
+                    >
+                      {word}
+                    </span>
+                  )}
+                </React.Fragment>
+              );
+            });
+
+            if (item.line.img.position >= words.length) {
+              let comma = null;
+              if (lineIndex === 0) {
+                if (language === "AR") {
+                  comma = <span className="order-1">,</span>;
+                } else {
+                  comma = <span className="order-2">,</span>;
+                }
+              }
+
+              textWithImage.push(
+                <div className="flex items-center">
                   <Avatar
+                    key="end-image"
                     className="hidden bg-black border-4 border-black dark:border-white sm:inline-block"
                     style={{
-                      mask: generateMask(wordIndex, true, lineIndex), // Apply mask to the image as well
-                      WebkitMask: generateMask(wordIndex, true, lineIndex),
+                      mask: generateMask(words.length, true, lineIndex),
+                      WebkitMask: generateMask(words.length, true, lineIndex),
                       boxShadow: "rgba(100, 100, 111, 0.2) 0px 7px 29px 0px",
                       width: w,
                       height: h,
                     }}
                   >
-                    <Carousel
-                      plugins={[plugin.current]}
-                      opts={{
-                        loop: true,
-                        align: "start",
-                        axis: "y",
-                        containScroll: "trimSnaps",
-                        skipSnaps: true,
-                      }}
-                      orientation="vertical"
-                      onMouseEnter={plugin.current.stop}
-                      onMouseLeave={() => plugin.current.play()}
-                    >
-                      <CarouselContent style={{ height: h }}>
-                        {item.line.img.images?.map((image, index) => (
-                          <CarouselItem
-                            className={`p-0 m-0 cursor-grab`}
-                            key={index}
-                          >
-                            <AvatarImage
-                              className="object-contain w-full h-full overflow-hidden"
-                              src={urlFor(image)
-                                .fit("max")
-                                .quality(80)
-                                .auto("format")
-                                .url()}
-                              alt={item.line.img.altText}
-                            />
-                          </CarouselItem>
-                        ))}
-                      </CarouselContent>
-                    </Carousel>
+                    <AvatarImage
+                      className={cn(lineIndex === 0 && "object-contain")}
+                      src={urlFor(item.line.img.images?.[0])
+                        .fit("fillmax")
+                        .quality(80)
+                        .auto("format")
+                        .url()}
+                      alt={item.line.img.altText}
+                    />
 
                     <AvatarFallback></AvatarFallback>
                   </Avatar>
-                )}
-
-                {item.line.highlight?.includes(wordIndex) ? (
-                  <em className="italic dark:text-white text-black -tracking-[0.04em] -z-10">
-                    {word}
-                  </em>
-                ) : (
-                  <span
-                    style={{
-                      mask: generateMask(wordIndex, false, lineIndex), // Apply dynamic mask for each word
-                      WebkitMask: generateMask(wordIndex, false, lineIndex),
-                    }}
-                  >
-                    {word}
-                  </span>
-                )}
-              </React.Fragment>
-            ));
-
-            // Check if image is at the end of the text (position > number of words)
-            if (item.line.img.position >= words.length) {
-              textWithImage.push(
-                <Avatar
-                  key="end-image"
-                  className="hidden bg-black border-4 border-black dark:border-white sm:inline-block"
-                  style={{
-                    mask: generateMask(words.length, true, lineIndex), // Apply mask to image at the end
-                    WebkitMask: generateMask(words.length, true, lineIndex), // Cross-browser support
-                    boxShadow: "rgba(100, 100, 111, 0.2) 0px 7px 29px 0px",
-                    width: w,
-                    height: h,
-                  }}
-                >
-                  <AvatarImage
-                    className={cn(lineIndex === 0 && "object-contain")}
-                    src={urlFor(item.line.img.images?.[0])
-                      .fit("fillmax")
-                      .quality(80)
-                      .auto("format")
-                      .url()}
-                    alt={item.line.img.altText}
-                  />
-                  <AvatarFallback>
-                    {/* <AvatarImage
-                      className=""
-                      src={urlFor(item.line.img.image)
-                        .fit("max")
-                        .quality(80)
-                        .format("jpg")
-                        .url()}
-                      alt={item.line.img.altText}
-                    /> */}
-                  </AvatarFallback>
-                </Avatar>
+                  {comma}
+                </div>
               );
             }
 
@@ -201,11 +198,10 @@ function Hero() {
           })}
         </motion.div>
 
-        {/* Description Section */}
         <motion.p
           variants={waterFall}
           className={cn(
-            "break-words whitespace-pre mx-auto  text-[1.05rem] text-balance dark:text-white/60 text-black/40 text-opacity-40 -tracking-[0.03] leading-[27.3px] font-mono text-center",
+            "break-words whitespace-pre mx-auto text-[1.05rem] text-balance dark:text-white/60 text-black/40 text-opacity-40 -tracking-[0.03] leading-[27.3px] font-mono text-center",
             language === "AR" && "text-lg font-baloo tracking-[0.125em]"
           )}
         >
@@ -213,7 +209,6 @@ function Hero() {
         </motion.p>
       </motion.div>
 
-      {/* scroll to projects Section */}
       <motion.div variants={waterFall}>
         <Link
           state={{ data: navLinks }}
@@ -229,14 +224,6 @@ function Hero() {
               size={60}
               strokeWidth={1}
             />
-            {/* <span
-              className={cn(
-                "text-xs dark:text-white animate-pulse opacity-20 font-unbounded",
-                language === "AR" && "font-baloo"
-              )}
-            >
-              {heroData.buttonText}
-            </span> */}
           </div>
         </Link>
       </motion.div>
