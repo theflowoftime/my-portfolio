d65612ef8ffaf2f578b0adb9def10d2b72d95c8d
theflowoftime <daflowoftime@outlook.com>
2024-12-14 11:23:50 +0100

    logging

--- api/contact-me.ts
+++ api/contact-me.ts
@@ -19,6 +19,7 @@ export default async function handler(req: VercelRequest, res: VercelResponse) {
   }
 
   const { recaptchaToken, ...formData } = req.body;
+  console.log(formData);
 
   const ip = retrieveIp(req);
 
